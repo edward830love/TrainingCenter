5c3be03f3c1c3cd3613e4c38ed659b4185cf8a65
Jiyang Wang <jiyang.wang.us@gmail.com>
2016-06-02 21:55:44 -0700

    third commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TrainingCenter
 This is a training center for Techbow LLC.
+
+1. To run this application, start your mongo server & do the following from the command line:
+2. bower install
+3. npm install
+4. nodemon server.js
--- server.js
+++ server.js
@@ -1,16 +1,46 @@
-var express = require('express');
+var express = require('express'),
+  stylus = require('stylus'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(str, path) {
+  return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(req, res){
-	res.render('index');
+mongoose.connect('mongodb://localhost/tc');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('trainingcenter db opened');
 });
 
-var port = 8888;
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+  res.render('index');
+});
+
+var port = process.env.PORT || 8886;
 app.listen(port);
-console.log('listening to port:' + port);
+console.log('Listening on port ' + port + '...');
